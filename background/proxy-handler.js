685406da2aa73d988383e7e2be1beb4e79c24fbd
SwitchyAlpha <switchyalpha@protonmail.com>
2021-07-11 15:59:51 +0800

    Always treat localhost/127.0.0.1 as in whitelist

--- background/proxy-handler.js
+++ background/proxy-handler.js
@@ -8,30 +8,36 @@ async function main() {
 
     // Main proxy handler
     function handleProxyRequest(requestInfo) {
-        function urlInWhitelist(url_str) {
-            const url = new URL(url_str);
-            return whitelist.has(url.hostname);
-        }
+        const url = new URL(requestInfo.url);
+        const hostname = url.hostname;
 
         const proxy_info_0 = {type: 'direct'};
         const proxy_info_1 = {type: 'socks', host: 'localhost', port: 1080};
 
+        // Always use direct for localhost
+        if (hostname == 'localhost' || hostname == '127.0.0.1') {
+            return proxy_info_0;
+        }
+
         // If url in whitelist
-        if (requestInfo.url && urlInWhitelist(requestInfo.url)) {
+        if (whitelist.has(hostname)) {
             return proxy_info_0;
         }
 
         // If request is from a tab
         if (requestInfo.tabId >= 0) {
             return browser.tabs.get(requestInfo.tabId).then(tab => {
-                if (tab.url && urlInWhitelist(tab.url)) {
-                    return proxy_info_0;
+                if (tab.url) {
+                    const tab_url = new URL(tab.url);
+                    if (whitelist.has(tab_url.hostname)) {
+                        return proxy_info_0;
+                    }
                 }
-                //console.log(`Using direct for ${requestInfo.url} because hostname of tab.url (${tab.url}) is in whitelist`);
+                //console.log(`Using direct for ${url} because hostname of tab.url (${tab.url}) is in whitelist`);
                 return proxy_info_1;
             });
         } else {  // tabId of initial requests or DNS requests is -1
-            //console.log(`Using proxy for ${requestInfo.url} because its hostname is not in whitelist and it does not belong to a tab`);
+            //console.log(`Using proxy for ${url} because its hostname is not in whitelist and it does not belong to a tab`);
             return proxy_info_1;
         }
     }
