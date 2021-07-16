26fafef4bad7f4488c0b0bb48cb3f2477dafbe41
SwitchyAlpha <switchyalpha@protonmail.com>
2021-07-16 09:54:32 +0800

    Add log about recording tab id

--- background/tab-url-recorder.js
+++ background/tab-url-recorder.js
@@ -3,6 +3,7 @@ export let tabUrls = {}
 function recordTabUrl(details) {
     console.log(`onBeforeNavigate to: ${details.url}`);
     if (details.frameId == 0 && !details.url.startsWith('about:')) {
+        console.log(`Recording tabId ${details.tabId} for ${details.url}`);
         tabUrls[details.tabId] = details.url;
     }
 }
